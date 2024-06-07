270e2100cd5ea117a41f190439208820483aff20
Pop <shounen.pop@gmail.com>
2024-06-07 13:34:03 +0700

    feat: add/delete data with api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,67 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const dataProduct = async () => {
+    try {
+      setLoading(true);
+      const datas = await axios.get("http://localhost:4001/products");
+      setProducts(datas.data.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    dataProduct();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
+        {loading ? (
+          <div className="app-wrapper">
+            <h1 className="app-title">Loading...</h1>
           </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
-
-          <button className="delete-button">x</button>
-        </div>
+        ) : (
+          products.map((product, index) => {
+            return (
+              <div className="product" key={product.id}>
+                <div className="product-preview">
+                  <img
+                    src={product.image}
+                    alt="some product"
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name: {product.name}</h1>
+                  <h2>Product price: {product.price} Baht</h2>
+                  <p>Product description: {product.description}</p>
+                </div>
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    const result = axios.delete(
+                      "http://localhost:4001/products/" + product.id
+                    );
+                    return setProducts(products.toSpliced(index, 1)), result;
+                  }}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
